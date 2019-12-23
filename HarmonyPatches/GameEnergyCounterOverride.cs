a390282d2c2be83af17816ab84d5a4b19c3ad34f
Jack <7891953+jackadamson@users.noreply.github.com>
2019-12-24 01:22:18 +1100

    Initial commit

---
+++ HarmonyPatches/GameEnergyCounterOverride.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using TMPro;
+
+/// <summary>
+/// See https://github.com/pardeike/Harmony/wiki for a full reference on Harmony.
+/// </summary>
+namespace BailOutReborn.HarmonyPatches
+{
+
+    [HarmonyPatch(typeof(GameEnergyCounter))]
+    [HarmonyPatch("AddEnergy")]
+    [HarmonyPatch(new Type[] { typeof(float) })]
+    class GameEnergyCounterOverride
+    {
+        static bool Prefix(ref GameEnergyCounter __instance, float value)
+        {
+            if (__instance.energy + value <= 1E-05f)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+﻿# BailOutReborn
+Plugin for Beat Saber to allow you to continue playing (without saving your score) on failure
